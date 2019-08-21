873718cc00155fa58ff0b83f33c71cdbd252a98e
Sahanasnayak <53560122+Sahanasnayak@users.noreply.github.com>
2019-08-21 20:35:42 +0530

    Add files via upload

---
+++ Homeauutomation_AMRVOICE_1.ino
@@ -0,0 +1,51 @@
+//SUCCESS
+#include<SoftwareSerial.h>
+int led1=3;
+int led2=4;
+const int txpin=1;
+const int rxpin=0;
+String readmsg;
+//char c;
+SoftwareSerial BT(rxpin,txpin);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+   BT.begin(9600);
+   pinMode(led1,OUTPUT);//TV
+  pinMode(led2,OUTPUT);//AC
+  
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(BT.available()){
+  delay(10);
+   char c=BT.read();
+  readmsg+=c;
+  }
+  if(readmsg.length()>0){
+if(readmsg=="*TV#"){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  delay(100);
+      Serial.println("TV is on");
+    }
+    else if(readmsg=="*AC#"){
+   digitalWrite(led2,HIGH);
+   digitalWrite(led1,LOW);
+   delay(100);
+    Serial.println("AC is on");
+   }
+   else if(readmsg=="*bye#"){
+     Serial.println("power saving mode");
+    digitalWrite(led2,HIGH); 
+    digitalWrite(led1,HIGH); 
+    delay(100);
+    }
+  
+    Serial.println(readmsg);
+    readmsg="";
+
+}
+}
